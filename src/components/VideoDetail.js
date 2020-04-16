a0a61b02fe87daa145cc33caf2a3e37d00725359
ycbencheng <bcyc2004@gmail.com>
2020-04-16 08:36:27 -0600

    Render clicked video

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,8 +5,13 @@ const VideoDetail = (props) => {
     return <div>Loading...</div>;
   }
 
+  const videoSrc = `https://www.youtube.com/embed/${props.video.id.videoId}`;
+
   return (
     <div>
+      <div class="ui embed">
+        <iframe src={videoSrc} />
+      </div>
       <div class="ui segment">
         <h4 class="ui header">{props.video.snippet.title}</h4>
         <p>{props.video.snippet.description}</p>
